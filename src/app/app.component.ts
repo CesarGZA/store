6f15f2d3e5aef0a2b7f9f1bde5f9103b3c0999cb
cesarGarza <cesarg_00@outlook.com>
2022-07-28 17:18:27 -0500

    Products component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  imgParent = '';
+
+
+
+  onLoaded(img: string){
+    console.log('log padre', img);
+
+  }
 }
